25416c836b2fbba443570d97eb3099ba466e8503
Cindy <jqsilen@163.com>
2024-08-03 21:31:00 +0800

    added 2 new class - BankAccount  && BankAccountHolder

---
+++ src/main/java/banking/BankAccount.java
@@ -0,0 +1,23 @@
+package banking;
+
+public class BankAccount extends Account{
+
+    // extends Account class for initiation
+
+    public BankAccount() {
+
+    }
+
+    public BankAccount(AccountHolder accountHolder, Long accountNumber, int pin, double startingDeposit) {
+        super(accountHolder, accountNumber, pin, startingDeposit);
+    }
+
+    public BankAccount (Company company, Long accountNumber, int pin, double startingDeposit){
+
+        super(company, accountNumber, pin, startingDeposit);
+
+    }
+
+
+
+}
---
+++ src/main/java/banking/BankAccountHolder.java
@@ -0,0 +1,11 @@
+package banking;
+
+public class BankAccountHolder extends AccountHolder{
+
+    public BankAccountHolder(int idNumber) {
+        super(idNumber);
+    }
+
+    public BankAccountHolder() {
+    }
+}
